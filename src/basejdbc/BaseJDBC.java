0781a59e1fe08b1a9cfae1fc2a462c223ae800c8
Juan <Juan@DESKTOP-J5PU6RU>
2019-06-11 09:44:54 +0200

    Inicio

---
+++ src/basejdbc/BaseJDBC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basejdbc;
+
+/**
+ *
+ * @author Juan
+ */
+public class BaseJDBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
