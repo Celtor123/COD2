da3420c1869b20f5276bf044adb3a97c8622030e
Juan <Juan@DESKTOP-J5PU6RU>
2019-06-11 14:16:30 +0200

    Metodo para insertar valores

--- src/basejdbc/BaseJDBC.java
+++ src/basejdbc/BaseJDBC.java
@@ -13,58 +13,66 @@ import java.util.logging.Logger;
  * @version 11/6/2019
  */
 public class BaseJDBC {
- //declaracion de atributos
-    static Connection conn = null;
-    static String url = null;
-    static Statement st;
-    static ResultSet rs;
+ 
   
      /**
       * Método que crea la tabla
       * @return 
       */
-    public static boolean crearTabla() {
-   
+    public static Connection crearTabla() {
+     /**declaracion de atributos
+      * 
+      */
+    Connection conn = null;
+    String url = null;
+    Statement st = null;
+    ResultSet rs;
     /**
     * @param conn,url,st,rs    
     *
     */
-     boolean execute = false;
+    
         try {
-       
-            // db parameters
-            url = "jdbc:sqlite:C:\\Users\\Juan\\PycharmProjects\\Base\\primera clase.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            
+            url = "jdbc:sqlite:primera clase.db";            
+            conn = DriverManager.getConnection(url);         
         st=conn.createStatement();
-        st.executeUpdate("CREATE TABLE IF NOT EXISTS TABLA (columna1 int,columna2 varchar,columna3 varchar)");
+        st.executeUpdate("CREATE TABLE IF NOT EXISTS TABLA (columna1 int PRIMARY KEY,columna2 varchar,columna3 varchar)");
            
         } 
          catch (SQLException ex) {
             Logger.getLogger(BaseJDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
           
-        finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
+//        finally {
+//            try {
+//                if (conn != null) {
+//                    conn.close();
+//                    st.close();
+//                }
+//            } catch (SQLException ex) {
+//                System.out.println("Error de creación de la tabla (TABLA)"+ex.getMessage());
+//            }
+//        }
+   return conn;   
+    }
+    
+    public static void insertar(Connection co,int columna1,String columna2,String columna3){
+        try {
+            Statement st = co.createStatement();
+            st.executeUpdate("INSERT INTO TABLA VALUES("+columna1+",'"+columna2+"','"+columna3+"')");
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseJDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
-  
-    System.out.println(execute);
-        return execute;
-}
+        
+    }
+    
   
     /**
      * @param args Método principal por los que pasan el resto
      */
     public static void main(String[] args) {
         crearTabla();
+       insertar(crearTabla(),2,"Hola","Celso");
     }
     
 }
